de47fed79946a15f725cf8d9643926d885806d26
konoha36 <cooper_palm@outlook.com>
2022-06-19 11:34:00 +0700

    3 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -20,7 +20,6 @@ export class ProductDetailsComponent implements OnInit {
 
     //Find the product that correspond with the id provided in route.
     this.product = products.find(
-      (product) => product.id === productIdFromRoute
-    );
+      product => product.id === productIdFromRoute);
   }
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,8 +8,6 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  products = products;
-
   share() {
     window.alert('The product has been shared!');
   }
