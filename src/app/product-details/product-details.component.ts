7b7d2e1c31cc38d35b0ddbf20aca718eda10f815
konoha36 <cooper_palm@outlook.com>
2022-06-24 11:27:11 +0700

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -10,7 +10,7 @@ import { Product, products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
-  /*...*/
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
